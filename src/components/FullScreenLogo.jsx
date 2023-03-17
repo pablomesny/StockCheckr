1af75553d8234eb2f0cf31b3e8c4fecfcac947e6
pablomesny <pablomesny@gmail.com>
2023-03-17 08:56:28 -0300

    FullScreen logo hotfix

--- src/components/FullScreenLogo.jsx
+++ src/components/FullScreenLogo.jsx
@@ -2,7 +2,7 @@ import { Container } from "@mui/material"
 
 export const FullScreenLogo = () => {
   return (
-    <Container sx={{ display: 'flex', height: '100vh', width: '100vw', objectFit: 'contain', bgcolor: '#0B3C5D', alignItems: 'center', justifyContent: 'center' }}>
+    <Container sx={{ display: 'flex', height: '100vh', minWidth: '100vw', objectFit: 'contain', bgcolor: '#0B3C5D', alignItems: 'center', justifyContent: 'center' }}>
         <img src="/src/assets/stockcheckr-login-logo.png" alt="brand logo" className="fullscreen-logo animate__animated animate__slower animate__fadeIn" />
     </Container>
   )
