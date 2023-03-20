5764a6db171ccb0cb5379ff4fddaab5fe48fe13a
pablomesny <pablomesny@gmail.com>
2023-03-19 21:38:24 -0300

    comments added into tabledata

--- src/components/TableData.jsx
+++ src/components/TableData.jsx
@@ -1,99 +1,83 @@
 import {
-    TableContainer,
-    Table,
-    TableHead,
-    TableRow,
-    TableBody,
-    styled,
-    TableCell,
-    Paper,
-    tableCellClasses,
-    TablePagination,
-    Box,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  styled,
+  TableCell,
+  Paper,
+  tableCellClasses,
+  TablePagination,
+  Box
 } from '@mui/material';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-        backgroundColor: theme.palette.primary.main,
-        color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-        fontSize: 14,
-    },
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14
+  }
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    '&:last-child td, &:last-child th': {
-        border: 0,
-    },
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover
+  },
+  '&:last-child td, &:last-child th': {
+    border: 0
+  }
 }));
 
 const createData = (name, calories, fat, carbs, protein) => {
-    return { name, calories, fat, carbs, protein };
+  return { name, calories, fat, carbs, protein };
 };
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9)
 ];
 
-export const TableData = () => {
-    return (
-        <Box sx={{ mx: 2, mt: 2}}>
-            <TableContainer component={ Paper }>
-                <Table aria-label="customized table">
-                    <TableHead>
-                        <TableRow>
-                            <StyledTableCell>
-                                Dessert (100g serving)
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                Calories
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                Fat&nbsp;(g)
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                Carbs&nbsp;(g)
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                Protein&nbsp;(g)
-                            </StyledTableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {
-                            rows.map(row => (
-                                <StyledTableRow key={row.name}>
-                                    <StyledTableCell component="th" scope="row">
-                                        {row.name}
-                                    </StyledTableCell>
-                                    <StyledTableCell align="right">
-                                        {row.calories}
-                                    </StyledTableCell>
-                                    <StyledTableCell align="right">
-                                        {row.fat}
-                                    </StyledTableCell>
-                                    <StyledTableCell align="right">
-                                        {row.carbs}
-                                    </StyledTableCell>
-                                    <StyledTableCell align="right">
-                                        {row.protein}
-                                    </StyledTableCell>
-                                </StyledTableRow>
-                            ))
-                        }
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            {/* <TablePagination
+// Titles received as params will be the headers of the table. They should be passed as an array.
+// Rows received as params will be part the body of the table. They should be passed as object/arrays.
+
+export const TableData = ({ /* titles, rows */ }) => {
+  return (
+    <Box sx={{ mx: 2, mt: 2 }}>
+      <TableContainer component={Paper}>
+        <Table aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+              <StyledTableCell align="right">Calories</StyledTableCell>
+              <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+              <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+              <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              rows.map(row => (
+                <StyledTableRow key={row.name}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.name}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{row.calories}</StyledTableCell>
+                  <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                  <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                  <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                </StyledTableRow>
+              ))
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {/* <TablePagination
                 rowsPerPageOptions={[5, 10, 25]}
                 component="div"
                 // count={rows.length}
@@ -102,6 +86,6 @@ export const TableData = () => {
                 // onPageChange={handleChangePage}
                 // onRowsPerPageChange={handleChangeRowsPerPage}
             /> */}
-        </Box>
-    );
+    </Box>
+  );
 };
--- src/pages/BrandsPage.jsx
+++ src/pages/BrandsPage.jsx
@@ -4,129 +4,128 @@ import { Modal } from '../components';
 import { TableData } from '../components/TableData';
 
 export const BrandsPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [ isOpen, setIsOpen ] = useState( false );
+  const handleToggleModal = () => {
+    setIsOpen(prev => !prev);
+  };
 
-    const handleToggleModal = () => {
-        setIsOpen( prev => !prev );
-    }
+  return (
+    <>
+      <Box
+        className="animate__animated animate__fadeIn"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
+          minHeight: '100%',
+          bgcolor: '#E5E5E5'
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: 3,
+            ml: 4,
+            py: 2
+          }}
+        >
+          <Typography component="h2" variant="h5">
+            Control panel
+          </Typography>
+          <Typography
+            component="h3"
+            variant="h6"
+            sx={{ fontWeight: 400, color: '#999999' }}
+          >
+            Brands
+          </Typography>
+          <Button
+            onClick={handleToggleModal}
+            variant="contained"
+            color="primary"
+            sx={{ display: 'flex', ml: 'auto', mr: 2 }}
+          >
+            Add brand
+          </Button>
+        </Box>
 
-    return (
-        <>
+        <Divider variant="middle" />
+
+        <Box
+          sx={{
+            display: 'flex',
+            flex: 1,
+            flexDirection: 'column',
+            height: '100%',
+            bgcolor: '#FAFAFA',
+            borderRadius: 1,
+            mx: 2,
+            mb: 1
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              margin: 2
+            }}
+          >
             <Box
-                className="animate__animated animate__fadeIn"
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    width: '100%',
-                    minHeight: '100%',
-                    bgcolor: '#E5E5E5',
-                }}
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                gap: 1
+              }}
             >
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        gap: 3,
-                        ml: 4,
-                        py: 2,
-                    }}
-                >
-                    <Typography component="h2" variant="h5">
-                        Control panel
-                    </Typography>
-                    <Typography
-                        component="h3"
-                        variant="h6"
-                        sx={{ fontWeight: 400, color: '#999999' }}
-                    >
-                        Brands
-                    </Typography>
-                    <Button
-                        onClick={ handleToggleModal }
-                        variant="contained"
-                        color="primary"
-                        sx={{ display: 'flex', ml: 'auto', mr: 2 }}
-                    >
-                        Add brand
-                    </Button>
-                </Box>
+              <Typography component="p" sx={{ fontSize: '1rem' }}>
+                Show
+              </Typography>
 
-                <Divider variant="middle" />
+              <TextField
+                style={{ width: 100 }}
+                size="small"
+                type="number"
+                defaultValue={10}
+              />
 
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flex: 1,
-                        flexDirection: 'column',
-                        height: '100%',
-                        bgcolor: '#FAFAFA',
-                        borderRadius: 1,
-                        mx: 2,
-                        mb: 1,
-                    }}
-                >
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            margin: 2,
-                        }}
-                    >
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                gap: 1,
-                            }}
-                        >
-                            <Typography component="p" sx={{ fontSize: '1rem' }}>
-                                Show
-                            </Typography>
-
-                            <TextField
-                                style={{ width: 100 }}
-                                size="small"
-                                type="number"
-                                defaultValue={10}
-                            />
-
-                            <Typography component="p" sx={{ fontSize: '1rem' }}>
-                                entries
-                            </Typography>
-                        </Box>
-
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                gap: 1,
-                            }}
-                        >
-                            <Typography component="p" sx={{ fontSize: '1rem' }}>
-                                Search:
-                            </Typography>
-
-                            <TextField size="small" type="text" />
-                        </Box>
-                    </Box>
-
-                    <Divider variant="middle" />
-
-                    <TableData />
-                </Box>
+              <Typography component="p" sx={{ fontSize: '1rem' }}>
+                entries
+              </Typography>
             </Box>
 
-            <Modal
-                isOpen={ isOpen }
-                handleToggleModal={ handleToggleModal }
-                title="Add brand"
-                inputLabel="Brand"
-            />
-        </>
-    );
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                gap: 1
+              }}
+            >
+              <Typography component="p" sx={{ fontSize: '1rem' }}>
+                Search:
+              </Typography>
+
+              <TextField size="small" type="text" />
+            </Box>
+          </Box>
+
+          <Divider variant="middle" />
+
+          <TableData />
+        </Box>
+      </Box>
+
+      <Modal
+        isOpen={isOpen}
+        handleToggleModal={handleToggleModal}
+        title="Add brand"
+        inputLabel="Brand"
+      />
+    </>
+  );
 };
